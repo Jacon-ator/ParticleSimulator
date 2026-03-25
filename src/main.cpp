c6319ac45589cf518ad956754c70ba9935c647d3
Jacob Kinchen <144300546+Jacon-ator@users.noreply.github.com>
2026-03-25 16:23:06 -0500

    Base Event Logic Implimentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
+
+	// Creates the main window
+	sf::RenderWindow window( sf::VideoMode( { 960, 540 } ), "SFML works!", sf::Style::Resize | sf::Style::Close);
+	// Initializes a circle called `shape`
+	sf::CircleShape shape( 200.f );
+	// Sets the color of `shape` to green
 	shape.setFillColor( sf::Color::Green );
 
+	// While application is open
 	while ( window.isOpen() )
 	{
+		// Process Events
 		while ( const std::optional event = window.pollEvent() )
 		{
-			if ( event->is<sf::Event::Closed>() )
+			// If the event is the close event, close the window
+			if ( event->is<sf::Event::Closed>() ) 
+			{
 				window.close();
+			}
+
+			// When window is resized, print size to console
+			if ( event->is<sf::Event::Resized>())
+			{
+				// std::cout << "Window Size = X: " << window.getSize().x << " Y: " << window.getSize().y << std::endl;
+				printf("Window Size = X: %i, Y: %i \n", window.getSize().x, window.getSize().y);
+			}
+
+			// If the user enters text, print that ascii to console
+			if (const auto* textEntered = event->getIf<sf::Event::TextEntered>())
+			{
+    			if (textEntered->unicode < 128)
+        			std::cout << "ASCII character typed: " << static_cast<char>(textEntered->unicode) << std::endl;
+			}
+			
 		}
 
+		// Clear the screen
 		window.clear();
+		// Draw `shape`
 		window.draw( shape );
+		// Update the window
 		window.display();
 	}
-}
+} 
